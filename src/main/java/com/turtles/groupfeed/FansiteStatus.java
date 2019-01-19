48d7f0be2cacaec9a130d7425b72ed2a956c30fb
turtles <turtlegerms@protonmail.com>
2019-01-18 22:55:13 -0800

    create FansiteStatus class

---
+++ src/main/java/com/turtles/groupfeed/FansiteStatus.java
@@ -0,0 +1,12 @@
+package com.turtles.groupfeed;
+
+import twitter4j.Status;
+
+public class FansiteStatus {
+
+    private Status status;
+
+    public FansiteStatus(Status status) {
+        this.status = status;
+    }
+}
