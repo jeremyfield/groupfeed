f7484765a0bcbdc4764c8c268c81ac869fee7405
turtles <turtlegerms@protonmail.com>
2019-01-21 00:55:54 -0800

    refresh command

--- src/main/java/com/turtles/groupfeed/Commands/RefreshCommand.java
+++ src/main/java/com/turtles/groupfeed/Commands/RefreshCommand.java
@@ -1,4 +1,25 @@
 package com.turtles.groupfeed.Commands;
 
-public class RefreshCommand {
+import com.turtles.groupfeed.GroupFeedBot;
+import com.turtles.groupfeed.TwitterUtils;
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import twitter4j.TwitterStream;
+
+import java.util.List;
+
+public class RefreshCommand implements Command {
+
+    private TwitterStream twitterStream;
+
+    public RefreshCommand(TwitterStream twitterStream) {
+        this.twitterStream = twitterStream;
+    }
+
+    @Override
+    public void runCommand(MessageReceivedEvent event, List<String> args) {
+        if(CommandUtils.isContributor(event)) {
+            TwitterUtils.restartStream(twitterStream);
+            GroupFeedBot.sendMessage(event.getChannel(), "Twitter stream restarted.");
+        }
+    }
 }
