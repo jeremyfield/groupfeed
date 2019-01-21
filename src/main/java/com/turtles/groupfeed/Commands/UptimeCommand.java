1edf08e193f3b985d3fe8efe18731b9d45d55620
turtles <turtlegerms@protonmail.com>
2019-01-20 23:17:10 -0800

    created uptime command class

--- src/main/java/com/turtles/groupfeed/Commands/UptimeCommand.java
+++ src/main/java/com/turtles/groupfeed/Commands/UptimeCommand.java
@@ -1,4 +1,22 @@
 package com.turtles.groupfeed.Commands;
 
-public class UptimeCommand {
+import com.turtles.groupfeed.GroupFeedBot;
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+
+public class UptimeCommand implements Command {
+
+    Instant createdAt;
+
+    public UptimeCommand() {
+        createdAt = Instant.now();
+    }
+
+    @Override
+    public void runCommand(MessageReceivedEvent event, List<String> args) {
+        GroupFeedBot.sendMessage(event.getChannel(), "Group Feed has been online for: " + Duration.between(createdAt, Instant.now()));
+    }
 }
